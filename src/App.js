ac02d2cfa7b9f453ffa33a677e7e3da9b76cb435
Bhakti199 <78597483+Bhakti199@users.noreply.github.com>
2022-03-14 12:39:10 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,59 +5,6 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <div>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores atque
-        reiciendis eum, minus harum perspiciatis commodi nihil corrupti id
-        aliquid ipsum, adipisci sunt eos debitis a tempora inventore voluptate
-        doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Iure suscipit non itaque doloribus perspiciatis incidunt fuga totam
-        corrupti et quo dolorem voluptas unde saepe, assumenda tempore vitae
-        ipsa consectetur debitis. Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Maiores atque reiciendis eum, minus harum perspiciatis
-        commodi nihil corrupti id aliquid ipsum, adipisci sunt eos debitis a
-        tempora inventore voluptate doloremque. Lorem ipsum dolor sit amet
-        consectetur adipisicing elit. Iure suscipit non itaque doloribus
-        perspiciatis incidunt fuga totam corrupti et quo dolorem voluptas unde
-        saepe, assumenda tempore vitae ipsa consectetur debitis. Lorem ipsum
-        dolor sit amet consectetur adipisicing elit. Maiores atque reiciendis
-        eum, minus harum perspiciatis commodi nihil corrupti id aliquid ipsum,
-        adipisci sunt eos debitis a tempora inventore voluptate doloremque.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure suscipit
-        non itaque doloribus perspiciatis incidunt fuga totam corrupti et quo
-        dolorem voluptas unde saepe, assumenda tempore vitae ipsa consectetur
-        debitis. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Maiores atque reiciendis eum, minus harum perspiciatis commodi nihil
-        corrupti id aliquid ipsum, adipisci sunt eos debitis a tempora inventore
-        voluptate doloremque. Lorem ipsum dolor sit amet consectetur adipisicing
-        elit. Iure suscipit non itaque doloribus perspiciatis incidunt fuga
-        totam corrupti et quo dolorem voluptas unde saepe, assumenda tempore
-        vitae ipsa consectetur debitis. Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Maiores atque reiciendis eum, minus harum perspiciatis
-        commodi nihil corrupti id aliquid ipsum, adipisci sunt eos debitis a
-        tempora inventore voluptate doloremque. Lorem ipsum dolor sit amet
-        consectetur adipisicing elit. Iure suscipit non itaque doloribus
-        perspiciatis incidunt fuga totam corrupti et quo dolorem voluptas unde
-        saepe, assumenda tempore vitae ipsa consectetur debitis. Lorem ipsum
-        dolor sit amet consectetur adipisicing elit. Maiores atque reiciendis
-        eum, minus harum perspiciatis commodi nihil corrupti id aliquid ipsum,
-        adipisci sunt eos debitis a tempora inventore voluptate doloremque.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure suscipit
-        non itaque doloribus perspiciatis incidunt fuga totam corrupti et quo
-        dolorem voluptas unde saepe, assumenda tempore vitae ipsa consectetur
-        debitis. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Maiores atque reiciendis eum, minus harum perspiciatis commodi nihil
-        corrupti id aliquid ipsum, adipisci sunt eos debitis a tempora inventore
-        voluptate doloremque. Lorem ipsum dolor sit amet consectetur adipisicing
-        elit. Iure suscipit non itaque doloribus perspiciatis incidunt fuga
-        totam corrupti et quo dolorem voluptas unde saepe, assumenda tempore
-        vitae ipsa consectetur debitis. Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Maiores atque reiciendis eum, minus harum perspiciatis
-        commodi nihil corrupti id aliquid ipsum, adipisci sunt eos debitis a
-        tempora inventore voluptate doloremque. Lorem ipsum dolor sit amet
-        consectetur adipisicing elit. Iure suscipit non itaque doloribus
-        perspiciatis incidunt fuga totam corrupti et quo dolorem voluptas unde
-        saepe, assumenda tempore vitae ipsa consectetur debitis.
-      </div>
     </div>
   );
 }
