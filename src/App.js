0ef690bc730bb1bca6b6135cc0c48e0875410a66
Bhakti199 <78597483+Bhakti199@users.noreply.github.com>
2022-03-14 12:23:41 +0530

    update: add text t test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import "./App.css";
-import logo from "./logo.png";
 import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores atque
+        reiciendis eum, minus harum perspiciatis commodi nihil corrupti id
+        aliquid ipsum, adipisci sunt eos debitis a tempora inventore voluptate
+        doloremque.
+      </div>
     </div>
   );
 }
