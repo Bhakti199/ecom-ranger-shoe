b367cbea3936a864ec5d9e01d83df3d9558279a2
Bhakti199 <78597483+Bhakti199@users.noreply.github.com>
2022-03-14 10:33:03 +0530

    feat: added navbar

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import logo from "./logo.png";
+import Navbar from "./Components/Navbar/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
