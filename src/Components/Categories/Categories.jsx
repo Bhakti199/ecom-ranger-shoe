5c1ab1ab93ebfac25313d66676c3d721b50f6788
Bhakti199 <78597483+Bhakti199@users.noreply.github.com>
2022-03-16 18:52:11 +0530

    update: improve syntax

--- src/Components/Categories/Categories.jsx
+++ src/Components/Categories/Categories.jsx
@@ -1,6 +1,4 @@
-import React from "react";
-import { useEffect } from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import "./Categories.css";
 import { Link } from "react-router-dom";
