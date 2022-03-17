83c4b70eea4834f9c9368384993eacdd8d5de59b
Bhakti199 <78597483+Bhakti199@users.noreply.github.com>
2022-03-17 12:48:42 +0530

    feat: add functionality to filter Sidebar

--- src/Components/FilterSidebar/FilterSidebar.jsx
+++ src/Components/FilterSidebar/FilterSidebar.jsx
@@ -1,12 +1,29 @@
 import "./FilterSidebar.css";
 import { AiFillStar } from "react-icons/ai";
+import { useFilter } from "../../Context/index";
+
 export const FilterSidebar = () => {
+  const { state, dispatch } = useFilter();
+
+  const BrandClickHandler = (event) => {
+    const checked = event.target.checked;
+    const typeBrand = event.target.value;
+    if (checked) {
+      return { type: "SET_BRAND", payload: typeBrand };
+    } else {
+      return { type: "UNSET_BRAND", payload: typeBrand };
+    }
+  };
   return (
     <form className="drawer">
       <div className="flex drawer-header">
         <p>FILTER</p>
         <p>
-          <button className="clear-btn button" type="reset">
+          <button
+            className="button"
+            type="reset"
+            onClick={() => dispatch({ type: "CLEAR_ALL_FILTERS" })}
+          >
             Clear All
           </button>
         </p>
@@ -21,6 +38,9 @@ export const FilterSidebar = () => {
             step={1000}
             defaultValue={5000}
             className="slider"
+            onChange={(event) =>
+              dispatch({ type: "FILTER_BY_RANGE", payload: event.target.value })
+            }
           />
         </div>
       </div>
@@ -31,14 +51,24 @@ export const FilterSidebar = () => {
           <ul className="list margin-top-bottom-zero flex-col sort-by-list">
             <li className="flex-row sort-by-item">
               <label>
-                <input type="radio" name="price-sort" />
+                <input
+                  type="radio"
+                  name="price-sort"
+                  checked={state.sortByPrice === "lowToHigh"}
+                  onChange={() => dispatch({ type: "PRICE_LOW_TO_HIGH" })}
+                />
                 Price- low to high
               </label>
             </li>
             <li className="flex-row sort-by-item">
               <label>
                 {" "}
-                <input type="radio" name="price-sort" />
+                <input
+                  type="radio"
+                  name="price-sort"
+                  checked={state.sortByPrice === "highToLow"}
+                  onChange={() => dispatch({ type: "PRICE_HIGH_TO_LOW" })}
+                />
                 Price- high to low
               </label>
             </li>
@@ -54,7 +84,13 @@ export const FilterSidebar = () => {
           <ul className="list margin-top-bottom-zero flex-col sort-by-list">
             <li className="flex-row sort-by-item">
               <label>
-                <input type="checkbox" name="inStock" className="checkbox" />
+                <input
+                  type="checkbox"
+                  name="inStock"
+                  className="checkbox"
+                  checked={state.includeOutOfStock === "include"}
+                  onChange={() => dispatch({ type: "INCLUDE_OUT_OF_STOCK" })}
+                />
                 Include out of stock
               </label>
             </li>
@@ -64,6 +100,8 @@ export const FilterSidebar = () => {
                   type="checkbox"
                   name="fastDelivery"
                   className="checkbox"
+                  checked={state.includeFastDelivery === "include"}
+                  onChange={() => dispatch({ type: "INCLUDE_FAST_DELIVERY" })}
                 />
                 Fast Delivery only
               </label>
@@ -75,26 +113,47 @@ export const FilterSidebar = () => {
       <div className="drawer-item-wrapper flex-col">
         <p className="margin-top-bottom-zero filter-heading">RATING</p>
         <div>
-          <p className="margin-top-bottom-zero"> clear ratings</p>
+          <p
+            className="margin-top-bottom-zero"
+            onClick={() => dispatch({ type: "UNSET_RATINGS" })}
+          >
+            {" "}
+            clear ratings
+          </p>
           <ul className="list  flex-col sort-by-list">
             <li className="flex-row sort-by-item">
               <label>
-                <input type="radio" name="rating" />4{" "}
-                <AiFillStar className="star-filter" /> and above
+                <input
+                  type="radio"
+                  name="rating"
+                  checked={state.sortByRatings === "fourPlus"}
+                  onChange={() => dispatch({ type: "RATINGS_FOUR_PLUS" })}
+                />
+                4 <AiFillStar className="star-filter" /> and above
               </label>
             </li>
             <li className="flex-row sort-by-item">
               <label>
-                <input type="radio" name="rating" />3{" "}
-                <AiFillStar className="star-filter" />
+                <input
+                  type="radio"
+                  name="rating"
+                  checked={state.sortByRatings === "threeToFour"}
+                  onChange={() => dispatch({ type: "RATINGS_THREE_TO_FOUR" })}
+                />
+                3 <AiFillStar className="star-filter" />
                 to 4
                 <AiFillStar className="star-filter" />
               </label>
             </li>
             <li className="flex-row sort-by-item">
               <label>
-                <input type="radio" name="rating" />3{" "}
-                <AiFillStar className="star-filter" /> and below
+                <input
+                  type="radio"
+                  name="rating"
+                  checked={state.sortByRatings === "threeBelow"}
+                  onChange={() => dispatch({ type: "RATINGS_THREE_BELOW" })}
+                />
+                3 <AiFillStar className="star-filter" /> and below
               </label>
             </li>
           </ul>
@@ -107,13 +166,23 @@ export const FilterSidebar = () => {
           <ul className="list margin-top-bottom-zero flex-col sort-by-list">
             <li className="flex-row sort-by-item">
               <label>
-                <input type="checkbox" className="checkbox" value="Puma" />
+                <input
+                  type="checkbox"
+                  className="checkbox"
+                  value="Puma"
+                  onChange={(event) => dispatch(BrandClickHandler(event))}
+                />
                 Puma
               </label>
             </li>
             <li className="flex-row sort-by-item">
               <label>
-                <input type="checkbox" className="checkbox" value="Adidas" />
+                <input
+                  type="checkbox"
+                  className="checkbox"
+                  value="Adidas"
+                  onChange={(event) => dispatch(BrandClickHandler(event))}
+                />
                 Adidas
               </label>
             </li>
@@ -124,20 +193,31 @@ export const FilterSidebar = () => {
                   name="reebok"
                   value="Nike"
                   className="checkbox"
+                  onChange={(event) => dispatch(BrandClickHandler(event))}
                 />
                 Nike
               </label>
             </li>
             <li className="flex-row sort-by-item">
               <label>
-                <input type="checkbox" value="Reebok" className="checkbox" />
+                <input
+                  type="checkbox"
+                  value="Reebok"
+                  className="checkbox"
+                  onChange={(event) => dispatch(BrandClickHandler(event))}
+                />
                 Reebok
               </label>
             </li>
             <li className="flex-row sort-by-item">
               <label>
                 {" "}
-                <input type="checkbox" value="Hrx" className="checkbox" />
+                <input
+                  type="checkbox"
+                  value="Hrx"
+                  className="checkbox"
+                  onChange={(event) => dispatch(BrandClickHandler(event))}
+                />
                 HRX by Hritik Roshan
               </label>
             </li>
---
+++ src/Context/FilterContext/FilterReducer.js
@@ -0,0 +1,88 @@
+export const filterManagement = (state, action) => {
+  switch (action.type) {
+    case "FILTER_BY_RANGE":
+      return {
+        ...state,
+        sortByRange: action.payload,
+      };
+    case "PRICE_LOW_TO_HIGH": {
+      return {
+        ...state,
+        sortByPrice: "lowToHigh",
+      };
+    }
+
+    case "PRICE_HIGH_TO_LOW": {
+      return {
+        ...state,
+        sortByPrice: "highToLow",
+      };
+    }
+    case "UNSET_PRICE":
+      return {
+        ...state,
+        sortByPrice: "",
+      };
+    case "INCLUDE_OUT_OF_STOCK": {
+      return {
+        ...state,
+        includeOutOfStock:
+          state.includeOutOfStock === "include" ? "exclude" : "include",
+      };
+    }
+
+    case "INCLUDE_FAST_DELIVERY": {
+      return {
+        ...state,
+        includeFastDelivery:
+          state.includeFastDelivery === "include" ? "exclude" : "include",
+      };
+    }
+    case "RATINGS_FOUR_PLUS":
+      return {
+        ...state,
+        sortByRatings: "fourPlus",
+      };
+
+    case "RATINGS_THREE_TO_FOUR":
+      return {
+        ...state,
+        sortByRatings: "threeToFour",
+      };
+
+    case "RATINGS_THREE_BELOW":
+      return {
+        ...state,
+        sortByRatings: "threeBelow",
+      };
+
+    case "UNSET_RATINGS":
+      return {
+        ...state,
+        sortByRatings: "",
+      };
+    case "SET_BRAND":
+      return {
+        ...state,
+        sortByBrand: [...state.sortByBrand, action.payload],
+      };
+    case "UNSET_BRAND":
+      return {
+        ...state,
+        sortByBrand: state.sortByBrand.filter(
+          (brandName) => brandName !== action.payload
+        ),
+      };
+    case "CLEAR_ALL_FILTERS":
+      return {
+        sortByRange: 0,
+        sortByPrice: "",
+        sortByBrand: [],
+        includeOutOfStock: "include",
+        includeFastDelivery: "",
+        sortByRatings: "",
+      };
+    default:
+      return state;
+  }
+};
