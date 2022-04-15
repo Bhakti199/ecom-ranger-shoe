e46e778bc980e1bf20883adbcf50c7d81a89f6bc
Bhakti199 <78597483+Bhakti199@users.noreply.github.com>
2022-04-15 23:13:31 +0530

    feat: wishlist and cart ui

--- src/Pages/CartPage/CartPage.jsx
+++ src/Pages/CartPage/CartPage.jsx
@@ -2,15 +2,23 @@ import React from "react";
 import "./CartPage.css";
 import { ProductCardForCart, BillCardForCart } from "../../Components/Index";
 import { useFilter } from "../../Context/index";
+import { Link } from "react-router-dom";
 export const CartPage = () => {
   const { state } = useFilter();
   const { cartList } = state;
   return (
-    <div className="landing-page bg-color">
-      <h2 className="cart-page-title">
-        {cartList.length > 0 ? "my cart" : "0 items in cart, Shop Now..."}
-      </h2>
-
+    <div className="cart-page-container bg-color">
+      <h2 className="cart-page-title">CART</h2>
+      {cartList.length === 0 && (
+        <div className="cart-empty-msg">
+          <span className="cart-empty-msg-title">
+            YOUR SHOPPING CART IS EMPTY!
+          </span>
+          <Link to="/product-listing">
+            <button className="cart-msg-btn">Shop Now</button>
+          </Link>
+        </div>
+      )}
       <div className="cart-page flex">
         <div className="cart-item-container flex-col">
           <ProductCardForCart />
